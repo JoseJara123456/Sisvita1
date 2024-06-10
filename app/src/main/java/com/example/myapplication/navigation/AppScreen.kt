455d4300d0991c4e2eea9cf3fb323074576fd7c9
JoseJara <149112727+JoseJara123456@users.noreply.github.com>
2024-06-09 20:34:17 -0500

    Update AppScreen.kt

--- app/src/main/java/com/example/myapplication/navigation/AppScreen.kt
+++ app/src/main/java/com/example/myapplication/navigation/AppScreen.kt
@@ -3,5 +3,6 @@ package com.example.myapplication.navigation
 sealed class AppScreen(val route: String){
     object HomeScreen: AppScreen("home")
     object MainScreen : AppScreen("main_screen")
-
+    object EstudianteScreen : AppScreen("estudiante")
+    object EspecialistaScreen : AppScreen("especialista")
 }
