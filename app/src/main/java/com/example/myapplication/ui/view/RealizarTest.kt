f8133953544c342d574d8c196d7b31c51c3a5c45
esthefanie <esthefanie.perez@unmsm.edu.pe>
2024-06-14 19:20:28 -0500

    Interfaz menu test

--- app/src/main/java/com/example/myapplication/ui/view/RealizarTest.kt
+++ app/src/main/java/com/example/myapplication/ui/view/RealizarTest.kt
@@ -1,51 +1,51 @@
 package com.example.myapplication.ui.view
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.example.myapplication.data.UserSession
-import com.example.myapplication.navigation.AppScreen
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.graphics.RectangleShape
-
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.myapplication.ui.viewmodel.LoginViewModel
-import com.example.myapplication.ui.viewmodel.TestViewModel
-
-import androidx.compose.runtime.*
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.data.model.TestsYPreguntasResponse
-import androidx.compose.material3.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import com.example.myapplication.data.model.beckOptions
-import com.example.myapplication.data.model.hamAOptions
 import com.example.myapplication.data.model.gad7Options
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-
-
+import com.example.myapplication.data.model.hamAOptions
+import com.example.myapplication.ui.viewmodel.TestViewModel
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -55,7 +55,6 @@ fun RealizarTest(navController: NavController) {
     viewModel.obtenerTestsYPreguntasUsuario()
     val testsYPreguntas = viewModel.testsYPreguntas
     val errorMessage = viewModel.errorMessage
-
     // Estado para rastrear el test seleccionado
     var selectedTest by remember { mutableStateOf<TestsYPreguntasResponse.tipoTest?>(null) }
 
@@ -166,26 +165,38 @@ fun RealizarTest(navController: NavController) {
                             },
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(vertical = 8.dp),
-                            shape = RectangleShape,
-                            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFDFDBD7))
+                                .padding(vertical = 8.dp)
+                                .shadow(4.dp, shape = RoundedCornerShape(12.dp))
+                                .border(width = 3.dp, color = Color(0xFF8BA4E7), shape = RoundedCornerShape(12.dp)),
+                            shape = RoundedCornerShape(12.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0E8FE),contentColor = Color.Black)
                         ) {
-                            Column(
-                                modifier = Modifier.fillMaxWidth()
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier=Modifier.fillMaxWidth()
                             ) {
-                                Text(
-                                    text = test.nombre,
-                                    fontWeight = FontWeight.Bold,
-                                    color = Color.Black,
-                                    textAlign = TextAlign.Start,
-                                    modifier = Modifier.fillMaxWidth()
-                                )
-                                Text(
-                                    text = "Descripción: ${test.descripcion}",
-                                    color = Color.Black,
-                                    textAlign = TextAlign.Start,
-                                    modifier = Modifier.fillMaxWidth()
-                                )
+
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp)
+                                ) {
+                                    Text(
+                                        text = test.nombre,
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 20.sp,
+                                        color = Color.Black,
+                                        textAlign = TextAlign.Start,
+                                        modifier = Modifier.fillMaxWidth()
+                                    )
+                                    Text(
+                                        text = test.descripcion,
+                                        fontSize = 18.sp,
+                                        color = Color(0xFF1E1F22),
+                                        textAlign = TextAlign.Start,
+                                        modifier = Modifier.fillMaxWidth()
+                                    )
+                                }
                             }
                         }
                     }
