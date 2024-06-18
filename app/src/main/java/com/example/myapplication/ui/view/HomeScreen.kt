fc92e6ad553111cbe109bf963dadeeface65c65a
Christian1224 <christian.machado@unmsm.edu.pe>
2024-06-18 02:08:24 -0500

    fronted del login

--- app/src/main/java/com/example/myapplication/ui/view/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/view/HomeScreen.kt
@@ -2,6 +2,9 @@
 
 package com.example.myapplication.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,7 +23,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -31,6 +37,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.R
 import com.example.myapplication.navigation.AppScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.ui.viewmodel.LoginViewModel
@@ -65,35 +72,59 @@ fun HomeScreen(navController: NavController) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .padding(16.dp),
+                //.padding(16.dp)
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(Color(0xFF0D47A1), Color(0xFF512DA8))
+                    )
+                    ),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.stack),
+                contentDescription = "sisvita-transformed",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                contentScale = ContentScale.Fit
+            )
             Text(
                 text = "Bienvenido a Sisvita",
                 fontSize = 32.sp,
                 fontWeight = FontWeight.Bold,
+                color = Color.White,
                 modifier = Modifier.padding(bottom = 16.dp)
             )
             // Campo de texto para el correo
             OutlinedTextField(
                 value = viewModel.email,
                 onValueChange = { viewModel.email = it },
-                label = { Text("Correo") },
+                label = { Text("Correo",color = Color.White) },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 16.dp)
+                    .padding(bottom = 16.dp),
+                colors = androidx.compose.material3.TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.White,
+                    cursorColor = Color.White
+            )
             )
             // Campo de texto para la contraseña
             OutlinedTextField(
                 value = viewModel.password,
                 onValueChange = { viewModel.password = it },
-                label = { Text("Contraseña") },
+                label = { Text("Contraseña",color = Color.White) },
                 visualTransformation = PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 16.dp)
+                    .padding(bottom = 16.dp),
+                colors = androidx.compose.material3.TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.White,
+                    cursorColor = Color.White
+            )
             )
             // Botón de login
             Button(
@@ -102,11 +133,12 @@ fun HomeScreen(navController: NavController) {
                         viewModel.login() // Llamar al método de login en el ViewModel
                     }
                 },
-                modifier = Modifier.fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(Color(0xFF085394)),
+                modifier = Modifier.fillMaxWidth()
+                .border(1.dp, Color.White),
+            colors = ButtonDefaults.buttonColors(Color(0xFF085394)),
                 enabled = viewModel.email.isNotEmpty() && viewModel.password.isNotEmpty() // Habilitar solo si ambos campos están llenos
             ) {
-                Text("Login")
+                Text("Login", color = Color.White)
             }
             // Navegar a la pantalla correspondiente si el login fue exitoso
             loginResponse?.let {
