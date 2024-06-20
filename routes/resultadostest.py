a604496aa955e5f09322fe7f93b7f7737771248a
Jose <xjoseduran01@gmail.com>
2024-06-20 17:39:24 -0500

    servicio test realizados

--- routes/resultadostest.py
+++ routes/resultadostest.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from models.test_realizado import testRealizados
+from models.usuarios import Usuarios
+from models.tests import Tests
 from utils.db import db
 from datetime import datetime
 
@@ -14,12 +16,20 @@ def vertestrealizado():
         lista_tests = []
         
         for test in tests:
+            # Obtener el nombre del usuario asociado al estudiante_id
+            usuario = Usuarios.query.get(test.usuarioid)
+            nombre_usuario = usuario.nombre if usuario else None
+            
+            # Obtener el nombre del test asociado al tipotest_id
+            test_obj = Tests.query.get(test.tipotest_id)
+            nombre_test = test_obj.nombre if test_obj else None
+            
             # Crear un diccionario para cada test realizado y serializarlo
             test_data = {
                 'test_id': test.test_id,
                 'fecha_test': test.fecha_test.strftime('%Y-%m-%d') if test.fecha_test else None,
-                'tipotest_id': test.tipotest_id,
-                'estudiante_id': test.estudiante_id,
+                'nombre_usuario': nombre_usuario,
+                'nombre_test': nombre_test,
                 'puntaje': test.puntaje
             }
             lista_tests.append(test_data)
