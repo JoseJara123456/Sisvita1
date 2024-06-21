1d125e438a205730d3113cd066e17f7fcc23ce03
Christian1224 <christian.machado@unmsm.edu.pe>
2024-06-21 02:18:08 -0500

    Filtro por fecha y tipo de Test

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes.login import login_bp
 from routes.registro import registro_bp
 from routes.test import test_bp
 from routes.resultadostest import testrealizado_bp
+from routes.test_fecha import test_fecha_id_bp
 from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_CONNECTION_URI
 from flask_cors import CORS
@@ -28,6 +29,7 @@ app.register_blueprint(login_bp)
 app.register_blueprint(registro_bp) # funcion registro
 app.register_blueprint(test_bp)
 app.register_blueprint(testrealizado_bp)
+app.register_blueprint(test_fecha_id_bp)
 
 if __name__ == '__main__':
   app.run(port=5000)
