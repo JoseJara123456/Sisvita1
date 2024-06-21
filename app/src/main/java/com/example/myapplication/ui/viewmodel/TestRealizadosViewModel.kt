d922cfc22c6836837de6e1fe769d6e909b0eba8b
alexandra2911 <142641448+alexandra2911@users.noreply.github.com>
2024-06-21 15:46:37 -0500

    Actualización TestRealizadosViewModel.kt

--- app/src/main/java/com/example/myapplication/ui/viewmodel/TestRealizadosViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/TestRealizadosViewModel.kt
@@ -10,18 +10,26 @@ import com.example.myapplication.data.repository.TestRealizadosRepository
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
+// Clase que extiende de ViewModel, proporcionando un ámbito de datos persistente a lo largo del ciclo de vida de las vistas asociadas
 class TestRealizadosViewModel : ViewModel() {
 
-    var testsYRespuestas by mutableStateOf<List<TestsRealizadosResponse.TestReali>?>(null)
-    var errorMessage by mutableStateOf("")
+    // Propiedades observables que se actualizarán en la UI cuando cambien
+    var testsYRespuestas by mutableStateOf<List<TestsRealizadosResponse.TestReali>?>(null)  // Lista de tests realizados
+    var errorMessage by mutableStateOf("")  // Mensaje de error para mostrar en la UI
+
+    // Instancia del repositorio que contiene métodos para obtener datos de la API
     private val testRealizadosRepository = TestRealizadosRepository()
 
+    // Función para obtener los tests realizados. Se lanza dentro del ámbito del ViewModel para manejar la corutina
     fun obtenerTestsRealizados() {
         viewModelScope.launch {
             try {
+                // Intentar obtener los datos desde el repositorio
                 val response = testRealizadosRepository.obtenerTestsRealizados()
+                // Si es exitoso, actualizar la lista de tests realizados
                 testsYRespuestas = response.data
             } catch (e: HttpException) {
+                // En caso de error HTTP, capturar la excepción y actualizar el mensaje de error
                 errorMessage = e.message ?: "An error occurred"
             }
         }
