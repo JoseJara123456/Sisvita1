965e0fb835e9949455c0ad590e932e45c3e30e80
JoseJara <149112727+JoseJara123456@users.noreply.github.com>
2024-06-21 10:58:26 -0500

    Update respuestas.py

--- routes/respuestas.py
+++ routes/respuestas.py
@@ -6,11 +6,15 @@ from datetime import datetime
 
 respuestas_bp = Blueprint('respuestas', __name__)
 
-@respuestas_bp.route('/enviarRespuestas/<int:usuario_id>', methods=['POST'])
-def enviar_respuestas(usuario_id):
+@respuestas_bp.route('/enviarRespuestas', methods=['POST'])
+def enviar_respuestas():
     try:
         data = request.json
+        usuario_id = data.get('usuarioId')
         respuestas = data.get('respuestas', [])
+
+        if not usuario_id or not respuestas:
+            return jsonify({'message': 'Bad request: usuarioId o respuestas faltantes', 'status': 400}), 400
 
         total_puntaje = 0
 
@@ -27,7 +31,10 @@ def enviar_respuestas(usuario_id):
         # Obtener tipotest_id de la primera opción (suponiendo que todas tienen el mismo tipotest_id)
         primer_opcion_id = respuestas[0]['opcionId']
         opcion = Opcion.query.filter_by(opcionid=primer_opcion_id).first()
-        tipotest_id = opcion.tipotest_id if opcion else None
+        if opcion is None:
+            return jsonify({'message': 'Bad request: Opción no encontrada', 'status': 400}), 400
+
+        tipotest_id = opcion.tipotest_id
 
         # Guardar en la tabla `test_realizados` una vez, fuera del bucle
         test_realizado = testRealizados(
