76a6eb7a807743c948db1afe0cbf08b97c1d23fc
alexandra2911 <142641448+alexandra2911@users.noreply.github.com>
2024-06-21 15:39:01 -0500

    Actualización TestRealizados.kt

--- app/src/main/java/com/example/myapplication/data/model/TestRealizados.kt
+++ app/src/main/java/com/example/myapplication/data/model/TestRealizados.kt
@@ -1,16 +1,18 @@
 package com.example.myapplication.data.model
 
+// Definición de una clase de datos en Kotlin para manejar las respuestas de los tests realizados
 data class TestsRealizadosResponse(
-    val data: List<TestReali>,
-    val message: String,
-    val status: Int
+    val data: List<TestReali>,  // Lista de tests realizados, cada elemento es un 'TestReali'
+    val message: String,        // Mensaje descriptivo sobre la respuesta
+    val status: Int             // Código de estado HTTP para la respuesta
 ) {
+    // Clase de datos interna para representar un test realizado específico
     data class TestReali (
-        val fecha_test: String,
-        val nombre_test: String,
-        val nombre_usuario: String,
-        val puntaje: Int,
-        val test_id: Int
+        val fecha_test: String,     // Fecha en que se realizó el test
+        val nombre_test: String,    // Nombre del test
+        val nombre_usuario: String, // Nombre del usuario que realizó el test
+        val puntaje: Int,           // Puntaje obtenido en el test
+        val test_id: Int            // Identificador único del test
     )
 
 }
