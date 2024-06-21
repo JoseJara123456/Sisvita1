d9cc7b6450c2853c530f93cb8f53ddd3ced1c85d
alexandra2911 <142641448+alexandra2911@users.noreply.github.com>
2024-06-21 15:43:19 -0500

    Actualización AppScreen.kt

--- app/src/main/java/com/example/myapplication/navigation/AppScreen.kt
+++ app/src/main/java/com/example/myapplication/navigation/AppScreen.kt
@@ -1,11 +1,11 @@
 package com.example.myapplication.navigation
 
-sealed class AppScreen(val route: String){
-    object HomeScreen: AppScreen("home")
-    object MainScreen : AppScreen("main_screen")
-    object EstudianteScreen : AppScreen("estudiante")
-    object EspecialistaScreen : AppScreen("especialista")
-    object RealizarTest : AppScreen("test")
-    object MostrarTestRealizado : AppScreen("realizado")
-    
+sealed class AppScreen(val route: String) {
+
+    object HomeScreen : AppScreen("home") // Pantalla de inicio
+    object MainScreen : AppScreen("main_screen") // Pantalla principal
+    object EstudianteScreen : AppScreen("estudiante") // Pantalla para estudiantes
+    object EspecialistaScreen : AppScreen("especialista") // Pantalla para especialistas
+    object RealizarTest : AppScreen("test") // Pantalla para realizar tests
+    object MostrarTestRealizado : AppScreen("realizado") // Pantalla para mostrar tests realizados
 }
