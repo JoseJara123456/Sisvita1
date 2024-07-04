a1538eb0abe19b883774b1c22424cd43dc18fe79
esthefanie <esthefanie.perez@unmsm.edu.pe>
2024-07-04 18:31:40 -0500

    Visualizar Heatmap

--- app/src/main/java/com/example/myapplication/ui/view/HeatmapScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/view/HeatmapScreen.kt
@@ -2,13 +2,31 @@ package com.example.myapplication.ui.view
 
 import android.os.Bundle
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -23,6 +41,7 @@ import com.google.android.gms.maps.model.TileOverlayOptions
 import com.google.maps.android.heatmaps.HeatmapTileProvider
 import com.google.maps.android.heatmaps.WeightedLatLng
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HeatmapScreen(navController: NavHostController? = null) {
     val context = LocalContext.current
@@ -31,6 +50,14 @@ fun HeatmapScreen(navController: NavHostController? = null) {
 
     viewModel.getDataHeatMap()
     val heatMapData = viewModel.heatMapData
+
+    var filterLevel by remember { mutableStateOf("") }
+    var filterType by remember { mutableStateOf("") }
+
+    val filteredHeatMap = heatMapData?.filter { heatmap ->
+        (filterLevel.isEmpty() || heatmap.nivel_ansiedad.toString() == filterLevel) &&
+                (filterType.isEmpty() || getTestType(heatmap.test_id) == filterType)
+    } ?: emptyList()
 
     val lifecycleOwner = LocalLifecycleOwner.current
     DisposableEffect(lifecycleOwner) {
@@ -52,32 +79,97 @@ fun HeatmapScreen(navController: NavHostController? = null) {
         }
     }
 
-        AndroidView(
-        factory = { mapView },
-        modifier = Modifier.fillMaxSize(),
-        update = { mapView ->
-            mapView.getMapAsync(OnMapReadyCallback { googleMap ->
-                googleMap.uiSettings.isZoomControlsEnabled = true
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(-12.046374, -77.042793), 10f))
-                heatMapData?.let {
-                    val weightedLatLngs = it.map { heatmap ->
-                        WeightedLatLng(LatLng(heatmap.latitud, heatmap.longitud), heatmap.nivel_ansiedad.toDouble())
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Mapa de calor") },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController?.popBackStack()
+                    }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
                     }
-
-                    val provider = HeatmapTileProvider.Builder()
-                        .weightedData(weightedLatLngs)
-                        .radius(50)
-                        .opacity(1.0)
-                        .build()
-
-                    googleMap.addTileOverlay(TileOverlayOptions().tileProvider(provider))
-                } ?: run {
-                    Log.d("HeatmapScreen", "No hay datos para heatmap")
                 }
-
-            })
+            )
         }
-    )
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Filtros
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Top
+            ) {
+                // Filtro de nivel
+                Column(modifier = Modifier.weight(1f).padding(start = 8.dp)) {
+                    Text("Nivel:")
+                    FilterDropdown(
+                        selectedOption = filterLevel,
+                        onOptionSelected = { filterLevel = it },
+                        options = listOf("", "1", "2", "3")
+                    )
+                }
+                // Filtro de tipo de test
+                Column(modifier = Modifier.weight(1f).padding(start = 8.dp)) {
+                    Text("Tipo de Test:")
+                    FilterDropdown(
+                        selectedOption = filterType,
+                        onOptionSelected = { filterType = it },
+                        options = listOf("", "Test de Beck", "Test HAM-A", "Test GAD-7")
+                    )
+                }
+            }
+
+            AndroidView(
+                factory = { mapView },
+                modifier = Modifier.fillMaxSize(),
+                update = { mapView ->
+                    mapView.getMapAsync(OnMapReadyCallback { googleMap ->
+                        googleMap.uiSettings.isZoomControlsEnabled = true
+                        googleMap.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                LatLng(-12.046374, -77.042793), 10f
+                            )
+                        )
+                        if (filteredHeatMap.isNotEmpty()) {
+                            val weightedLatLngs = filteredHeatMap.map { heatmap ->
+                                WeightedLatLng(
+                                    LatLng(heatmap.latitud, heatmap.longitud),
+                                    heatmap.nivel_ansiedad.toDouble()
+                                )
+                            }
+
+                            val provider = HeatmapTileProvider.Builder()
+                                .weightedData(weightedLatLngs)
+                                .radius(50)
+                                .opacity(1.0)
+                                .build()
+
+                            googleMap.addTileOverlay(TileOverlayOptions().tileProvider(provider))
+                        } else {
+                            Log.d("HeatmapScreen", "No hay datos para heatmap")
+                        }
+                    })
+                }
+            )
+        }
+    }
+}
+
+fun getTestType(testId: Int): String {
+    return when (testId) {
+        1 -> "Test de Beck"
+        2 -> "Test HAM-A"
+        3 -> "Test GAD-7"
+        else -> ""
+    }
 }
 
 
--- app/src/main/java/com/example/myapplication/ui/view/MostrarTestRealizado.kt
+++ app/src/main/java/com/example/myapplication/ui/view/MostrarTestRealizado.kt
@@ -1,20 +1,16 @@
 package com.example.myapplication.ui.view
 
-import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -22,20 +18,18 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.RadioButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -45,10 +39,8 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.myapplication.data.model.TestsRealizadosResponse
+import com.example.myapplication.navigation.AppScreen
 import com.example.myapplication.ui.viewmodel.TestRealizadosViewModel
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.OutlinedTextField
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -142,6 +134,15 @@ fun MostrarTestRealizado(navController: NavController) {
                         options = listOf("", "Test de Beck", "Test HAM-A", "Test GAD-7")
                     )
                 }
+            }
+            Button(
+                onClick = {
+                    navController.navigate(AppScreen.HeatmapScreen.route)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(Color(0xFF085394))
+            ) {
+                Text("Ver mapa de calor")
             }
 
             LazyColumn(
