48df10a7a7c118c2fefea899ba463f1f6c21c1ba
JoseJara <149112727+JoseJara123456@users.noreply.github.com>
2024-07-05 02:27:32 -0500

    Update MostrarTestRealizado.kt

--- app/src/main/java/com/example/myapplication/ui/view/MostrarTestRealizado.kt
+++ app/src/main/java/com/example/myapplication/ui/view/MostrarTestRealizado.kt
@@ -1,16 +1,20 @@
 package com.example.myapplication.ui.view
 
+import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -18,18 +22,20 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -39,8 +45,10 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.myapplication.navigation.AppScreen
+import com.example.myapplication.data.model.TestsRealizadosResponse
 import com.example.myapplication.ui.viewmodel.TestRealizadosViewModel
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.OutlinedTextField
 
 
 
@@ -57,7 +65,6 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 import android.widget.DatePicker
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -118,7 +125,7 @@ fun MostrarTestRealizado(navController: NavController) {
                                 nombreTest = selectedTest.nombre_test,
                                 nombreUsuario = selectedTest.nombre_usuario,
                                 nivelAnsiedad = selectedTest.nivel_ansiedad,
-                                testId = selectedTest.test_id
+                                testId = selectedTest.test_id.toString()
                             )
                             viewModel.saveSelectedTestData(testData)
                             navController.navigate(AppScreen.EspecialistaFiltro.route)
@@ -183,15 +190,6 @@ fun MostrarTestRealizado(navController: NavController) {
                         onOptionSelected = { filterType = it }
                     )
                 }
-            }
-            Button(
-                onClick = {
-                    navController.navigate(AppScreen.HeatmapScreen.route)
-                },
-                modifier = Modifier.fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(Color(0xFF085394))
-            ) {
-                Text("Ver mapa de calor")
             }
 
 
