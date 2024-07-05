d01081c149ab1e70237570bcec64023ec68ff027
JoseJara <149112727+JoseJara123456@users.noreply.github.com>
2024-07-05 00:18:46 -0500

    Update TestRealizadosViewModel.kt

--- app/src/main/java/com/example/myapplication/ui/viewmodel/TestRealizadosViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/TestRealizadosViewModel.kt
@@ -12,18 +12,10 @@ import retrofit2.HttpException
 import java.io.IOException
 
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+
 import com.example.myapplication.data.SelectedTestData
-import com.example.myapplication.data.model.TestsRealizadosResponse
-import com.example.myapplication.data.repository.TestRealizadosRepository
 import com.example.myapplication.data.model.TestData
-import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import java.io.IOException
+
 
 
 class TestRealizadosViewModel : ViewModel() {
